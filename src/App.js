61bc80886d0ff5d39bb364f8bf2da4fd1db390b8
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2022-12-13 00:37:23 -0800

    updates to loading screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import React, { Suspense } from 'react'
+import { Audio } from  'react-loader-spinner'
+
+const AboutUs = React.lazy(() => import('../src/components/AboutUs'))
+const Packages = React.lazy(() => import('../src/components/Packages'))
 
 function App() {
   return (
@@ -9,8 +12,18 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+    <Suspense fallback={<Audio
+    height = "80"
+    width = "80"
+    radius = "9"
+    color = 'green'
+    ariaLabel = 'three-dots-loading'     
+    wrapperStyle
+    wrapperClass/> 
+  }>
           <AboutUs />
           <Packages />
+    </Suspense>
       </div>
     </div>
   );
